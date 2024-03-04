dc93f1d59f5677440ea3300c8500a918f66d011a
miakojason <miakojason@gmail.com>
2024-03-04 19:14:52 +0800

    跟住友男提點更新add api 完成

---
+++ back/add.php
@@ -0,0 +1,15 @@
+<?php
+$table = $_POST['table'];
+$DB = ${ucfirst($_GET['do'])};
+if (!empty($_FILES['img']['tmp_name'])) {
+    move_uploaded_file($_FILES['img']['tmp_name'], "../img/" . $_FILES['img']['name']);
+    $_POST['img'] = $_FILES['img']['name'];
+}
+if ($table != 'admin') {
+    $_POST['sh'] = ($table == 'title') ? 0 : 1;
+} else {
+    unset($_POST['pw']);
+}
+unset($_POST['table']);
+$DB->save($_POST);
+to("../back.php?do=$table");
--- back/title.php
+++ back/title.php
@@ -10,12 +10,31 @@
                     <td width="7%">刪除</td>
                     <td></td>
                 </tr>
+                <?php
+                $rows = $DB->all();
+                foreach ($rows as $row) {
+                ?>
+                    <tr>
+                        <td><img src="./img/<?=$row['img'];?>" style="width: 300px;height: 30px;"></td>
+                        <td><?=$row['text'];?></td>
+                        <td><input type="radio" name="sh" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>></td>
+                        <td>
+                            <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+                            <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+                        </td>
+                        <td>
+                            <input type="button" value="更新圖片" onclick="op('#cover','#cvr','./model/upload.php?table=<?=$do;?>&id=<?=$row['id'];?>')">
+                        </td>
+                    </tr>
+                <?php
+                }
+                ?>
             </tbody>
         </table>
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','view.php?do=title')" value="新增網站標題圖片"></td>
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./model/<?=$do;?>.php?table=<?=$do;?>')" value="新增網站標題圖片"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
             </tbody>
