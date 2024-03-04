02d60d5a183099badf36e9f443826f96252a9b31
miakojason <miakojason@gmail.com>
2024-03-04 09:55:39 +0800

    先新增內容到sql去

---
+++ api/1.sql
@@ -0,0 +1,11 @@
+INSERT INTO `image` (`id`, `img`, `sh`)
+ VALUES (NULL, '01D01.jpg', '1'),
+  (NULL, '01D02.jpg', '1'), 
+  (NULL, '01D03.jpg', '1'),
+   (NULL, '01D04.jpg', '1'),
+    (NULL, '01D05.jpg', '1'),
+    (NULL, '01D06.jpg', '1'),
+    (NULL, '01D07.jpg', '1'),
+    (NULL, '01D08.jpg', '1'),
+    (NULL, '01D09.jpg', '1'),
+     (NULL, '01D10.jpg', '1');
