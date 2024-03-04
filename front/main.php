b08e4bae9ea0ddeddc5aae6707ae5f9e9f1da2ff
miakojason <miakojason@gmail.com>
2024-03-04 13:41:54 +0800

    前台最新消息自己打出來o無吳看

--- front/main.php
+++ front/main.php
@@ -14,7 +14,7 @@
         $lins = $Mvim->all(['sh' => 1]);
         foreach ($lins as $lin) {
         ?>
-        lin.push('<?=$lin['img'];?>');
+            lin.push('<?= $lin['img']; ?>');
         <?php
         }
         ?>
@@ -35,8 +35,29 @@
     </script>
     <div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區
-        </span>
+    <?php
+    if($News->count(['sh'=>1])>5){
+        echo "<a href='?do=news'style='float:right'>more...</a>";
+    }
+    ?>
+    </span>
         <ul class="ssaa" style="list-style-type:decimal;">
+            <?php
+            $total = $News->count(['sh' => 1]);
+            $div = 5;
+            $pages = ceil($total / $div);
+            $now = $_GET['p'] ?? 1;
+            $start = ($now - 1) * $div;
+            $news = $News->all(['sh' => 1], " limit $start,$div");
+            foreach ($news as $idx => $new) {
+            ?>
+                <li start="<?= $idx; ?>">
+                    <div><?= mb_substr($new['text'], 0, 20); ?>...</div>
+                    <div class="all" style="display: none;"><?= $new['text']; ?></div>
+                </li>
+            <?php
+            }
+            ?>
         </ul>
         <div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
         <script>
